5f7e61ab48d33be7f1e9d7f29cbf16418991679f
shashankpy02 <shashank.py02@gmail.com>
2025-01-30 16:13:47 +0530

    adding real time dates

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## About 
 -currency exchange project, where users can convert value from 'one currency' to 'another currency'.<br/>
 -real time rates using api from 👇🏻<br/>
--url=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-01-28/v1/currencies/${currency}.json'<br/>
--just replace ${currency} from "usd"/"inr" or any currency based upon which you wants to compare rates.
+-url=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${getCurrentDate()}/v1/currencies/${currency}.json`<br/>
+-just replace ${currency} from "usd"/"inr" or any currency based upon which you wants to compare rates.<br/>
+-${getCurrentDate()} is used, to calculate dates in real time or you can just simply replace to any date in 'YYYY-MM-DD' format.
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
 
-// const getCurrentDate = () => {
-//   const today = new Date();
-//   const year = today.getFullYear();
-//   const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
-//   const day = String(today.getDate()).padStart(2, '0');
-//   return `${year}-${month}-${day}`;
-// };
 
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
-
+    
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-01-28/v1/currencies/${currency}.json`)
+        const getCurrentDate = () => {
+          const today = new Date();
+          const year = today.getFullYear();
+          const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+          const day = String(today.getDate()).padStart(2, '0');
+          return `${year}-${month}-${day}`;
+        };
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${getCurrentDate()}/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     },[currency])
